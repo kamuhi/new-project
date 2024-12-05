1e304fa2332b98561287c33357d8b7354bfd1623
kamuhi <it232092@trident.ac.jp>
2024-12-05 20:27:00 +0900

    グリッドフロアの周りを周回するようになった

--- Game/Player/Player.h
+++ Game/Player/Player.h
@@ -16,6 +16,7 @@ namespace mylib
 class Player
 {
 private:
+	
 	//共通リソース
 	CommonResources* m_commonResources;
 
@@ -27,14 +28,33 @@ private:
 
 	//ジオメトリックプリミティブ
 	std::unique_ptr<DirectX::GeometricPrimitive> m_teapot;
+	std::unique_ptr<DirectX::GeometricPrimitive> m_sphere;
 
 	//弾
 	std::unique_ptr<Bullets> m_bullets;
 
 	// 回転角（度）
 	float m_angle;
+	//ゴール番号
+	unsigned int m_currentGoalNo;
+
+	//ゴール配列
+	std::vector<DirectX::SimpleMath::Vector3> m_goals;
+	
 	// 座標
 	DirectX::SimpleMath::Vector3 m_position;
+	DirectX::SimpleMath::Vector3 m_spherePosition;
+
+	// 各ワールド行列
+	DirectX::SimpleMath::Matrix m_teapotWorld;
+	DirectX::SimpleMath::Matrix m_sphereWorld;
+
+	// 視界表現用境界球
+	DirectX::BoundingSphere m_boundingSphere;
+
+	// 発見フラグ
+	bool m_isInside;
+
 	// 速さ
 	static const float SPEED;
 
